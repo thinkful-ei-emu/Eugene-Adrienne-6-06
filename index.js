8d610e29ee6b21cfa3da35661bfbe3f109ac5654
geneggy <geneggy@gmail.com>
2019-06-06 13:59:15 -0700

    added cat drill

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+'use strict';
+
 $('.thumbnail').on('click', e => {
   console.log(e.target);
 });
+
+$('.thumbnail').on('click', e => {
+  console.log(e.target);
+});
+
+'use strict';
+function carousel() {
+  $('.thumbnail').on('click', e => {
+
+    // set selectedImg to img of current target
+    const selectedImg = $('img', $(event.currentTarget));
+
+    //set the src and alt of the .selected_img equal to the src and alt of the selected img.
+    $('.selected_img').attr('src', selectedImg.attr('src'));
+    $('.selected_img').attr('alt', selectedImg.attr('alt'));
+  });
+}
+
+$(carousel);
